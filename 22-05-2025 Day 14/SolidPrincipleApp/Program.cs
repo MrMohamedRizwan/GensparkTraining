b728e577a898f28e9d89f54f3fe90b973f7b8f25
MrMohamedRizwan <mohamedrizwan2207@gmail.com>
2025-05-22 17:20:24 +0530

    Day14 Task SOLID Principles

--- 22-05-2025 Day 14/SolidPrincipleApp/Program.cs
+++ 22-05-2025 Day 14/SolidPrincipleApp/Program.cs
@@ -13,46 +13,46 @@ class Program
     static void Main(string[] args)
     {
         //Single responsibilty
-        //var invoice = new Invoice { Id = 1, Amount = 1000.0 };
-        //var calculator=new InvoiceCalculator();
-        //var tax=calculator.CalculateTax(invoice);
-        //Console.WriteLine($"The Tax amount is {tax}");
+        var invoice = new Invoice { Id = 1, Amount = 1000.0 };
+        var calculator=new InvoiceCalculator();
+        var tax=calculator.CalculateTax(invoice);
+        Console.WriteLine($"The Tax amount is {tax}");
 
-        //var printer = new InvoicePrinter();
-        //printer.Print(invoice);
+        var printer = new InvoicePrinter();
+        printer.Print(invoice);
 
-        //var emailer = new InvoiceEmail();
-        //emailer.email(invoice);
+        var emailer = new InvoiceEmail();
+        emailer.email(invoice);
 
         // Open Closed Responsibility
-        //var p1 = new PaymentService(new UPIPayment());
-        //p1.MakePayment(100);
+        var p1 = new PaymentService(new UPIPayment());
+        p1.MakePayment(100);
 
-        //var p2 = new PaymentService(new ApplePayPayment());
-        //p2.MakePayment(200);
+        var p2 = new PaymentService(new ApplePayPayment());
+        p2.MakePayment(200);
 
         ////Liskov Substitution Principle
-        //Documents d = new ReadOnlyDocument();
-        //d.View();
-        //d.Edit(); //error
+        Documents d = new ReadOnlyDocument();//General Design
+        d.View();
+        d.Edit(); //error
 
 
-        //Documents dd = new EditableDocument();
-        //dd.View();
-        //if (dd is EditableDocument editable)
-        //{
-        //    editable.Edit();
-        //}
+        Documents dd = new EditableDocument();
+        dd.View();
+        if (dd is EditableDocument editable)
+        {
+           editable.Edit();
+        }
 
         ////Interface Segregation
-        //IPrinter oldPrinter = new OldPrinter();
-        //oldPrinter.Print();
+        IPrinter oldPrinter = new OldPrinter();
+        oldPrinter.Print();
 
-        //// Use NewMultiFunctionPrinter (supports all operations)
-        //NewPrinter newPrinter = new NewPrinter();
-        //newPrinter.Print();
-        //newPrinter.Scan();
-        //newPrinter.Fax();
+        // Use NewPrinter (supports all operations)
+        NewPrinter newPrinter = new NewPrinter();
+        newPrinter.Print();
+        newPrinter.Scan();
+        newPrinter.Fax();
 
         ////Dependency Inversion
         IMessageService messageService = new EmailService();
